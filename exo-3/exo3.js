5eaf31c06932f9021c6d34f68b440f4d0c449435
Ugo <ugwadoux@gmail.com>
2023-05-23 17:06:19 +0200

    Avancement exercice 3, getPlayerChoice créé

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,14 @@
 // you can write js here
-console.log('exo-3');
+console.log('exo-3')
+var playerInput = prompt("Your choice ( rock, paper, scissors) ?")
+ playerInput = playerInput.toLowerCase()
+
+function getPlayerChoice(playerInput){
+    if (playerInput=="rock" | playerInput == "paper" | playerInput =="scissors"){
+        console.log(playerInput)
+        return playerInput
+    }else{
+        console.log('Tu sais pas écrire recommence')
+    }
+}
+getPlayerChoice(playerInput)
