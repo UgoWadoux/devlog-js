a2e54a3c617fe4385f0e3a95cd12e525d1dc433f
Ugo <ugwadoux@gmail.com>
2023-05-24 11:49:27 +0200

    Ex04- Complet

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,16 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+let last = secretMessage.pop()
+//console.log(secretMessage)
+secretMessage.push("to", "program")
+//console.log(secretMessage)
+secretMessage[6] = "right"
+let first =secretMessage.shift()
+//console.log(secretMessage)
+secretMessage.unshift("Programming")
+//console.log(secretMessage)
+secretMessage.splice(5,5, "know")
+//console.log(secretMessage)
+//secretMessage.join()
+console.log(secretMessage.join())
