0660cae4178fb29ed01cfc9f017f1cca6c748d0d
Ugo <ugwadoux@gmail.com>
2023-05-24 16:53:23 +0200

    Ex06-avancement

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,25 @@ var joeInfo = {
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
-
+//console.log(joeInfo.cars.length)
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
-
+joeInfo['bathrooms'] = 1
+//console.log(joeInfo.bathrooms)
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+joeInfo['garage']= true
+//console.log(joeInfo.garage)
+var team ={}
+team ={
+    players:[{
+        firstName: "Pablo",
+        lastName: "Sanchez",
+        age: 11,
+    }],
+    games:[{
+        opponent: "Broncos",
+        teamPoints: 42,
+        opponentPoints: 27
+    }],
+}
+
+console.log(team)
