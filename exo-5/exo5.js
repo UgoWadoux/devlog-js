a9180f14363bd4430797be7246c280bf66963c8d
Ugo <ugwadoux@gmail.com>
2023-05-24 15:32:22 +0200

    Ex05-Complet

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,17 @@
 // you can write js here
 
 console.log('exo-5');
+let input = 'Bonjour la baleine'
+const vowels = ["A","E","I","O","U","Y"]
+const resultArray= []
+
+for (let i=0; i<input.length; i++){
+    //console.log(input[i])
+    for(let j=0; j<vowels.length; j++){
+        if(input[i].toUpperCase() == vowels[j]){
+            resultArray.push(input[i].toUpperCase())
+        }
+    }
+}
+
+console.log(resultArray.join(''))
